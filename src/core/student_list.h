8a2bf1b68ffe8f2bacd295d130831ba1291ff0bd
semiot <1047357938@qq.com>
2026-06-06 00:41:43 +0800

    re

--- src/core/student_list.h
+++ src/core/student_list.h
@@ -5,7 +5,8 @@
 #ifndef FOR_REVIEW_LINKED_LIST_H
 #define FOR_REVIEW_LINKED_LIST_H
 
-typedef struct StudentNode {
+typedef struct StudentInfo
+{
     int id;
     int stuRoomNum;
     char stuName[18];
@@ -14,19 +15,23 @@ typedef struct StudentNode {
     char stuBirth[20];
     char stuHome[100];
     int stuPhone;
-    struct StudentNode *nextNode_ptr;
-} StudentNode;
+}StudentInfo;//student data
 
-extern StudentNode *head;
+typedef struct StudentNode {
+    StudentInfo info;//student data
+    struct StudentNode *nextNode_ptr;//link ptr
+} StudentNode;//linked list node struct
 
-void listInit();
-StudentNode* createNode(int I_id, int I_roomNum, char I_name[],char I_sex[], char I_class[], char I_birth[], char I_home[],int I_phone);//"I" means input
-StudentNode* addNode_Tail(StudentNode* *head_ptr, int I_id, int I_roomNum, char I_name[],char I_sex[], char I_class[], char I_birth[], char I_home[],int I_phone);
-void modifyByPos(StudentNode* head_ptr, int pos, int I_id, int I_roomNum, char I_name[],char);
-void modifyById(StudentNode* head_ptr, int target_id, int I_id, int I_roomNum, char I_name);
-StudentNode* IterateOverList(StudentNode* head_ptr, int target_id);
-void deleteByPos(StudentNode* *head_ptr, int pos);
-void freeList();
+extern StudentNode *head_p;//list head pointer
+
+void listInit();//initialize list
+StudentNode* createNode(int I_id, int I_roomNum, char I_name[],char I_sex[], char I_class[], char I_birth[], char I_home[],int I_phone);//Create new student information("I" means input)
+StudentNode* addNode_Tail(StudentNode* *head_ptr, int I_id, int I_roomNum, char I_name[],char I_sex[], char I_class[], char I_birth[], char I_home[],int I_phone);//Add Student information to the end of the list
+void modifyByPos(StudentNode* head_ptr, int pos, int I_id, int I_roomNum, char I_name[],char I_sex[]);
+void modifyById(StudentNode* head_ptr, int target_id, int I_id, int I_roomNum, char I_name[], char I_sex[]);
+void IterateOverList(StudentNode* head_ptr, void(*visitFunc)(StudentNode* currentNode_p,void*), void* context);//Iterate over list,you can write a callback function to visit each node
+void deleteByPos(StudentNode* *head_ptr, int pos);//Delete Student info by position
+void freeList();//Free all student info in the list
 
 
 #endif //FOR_REVIEW_LINKED_LIST_H
